cc117ba4da60d3fba5ff4606e00e665c7a16c65b
Jhonata Polito <jhonata.demuner@gmail.com>
2023-03-12 15:47:00 -0300

    first update after refactoring

--- frontend/main.js
+++ frontend/main.js
@@ -1,139 +1,21 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+console.log("Bundle loaded");
 
-import './assets/style.css';
+const accountFormChanger = document.querySelector('#accountFormChanger');
+const chooserBar = document.querySelector('#chooserBar');
+const formSlider = document.querySelector('#formSlider');
 
-// const schedule = require('./modules/scheduleModule.js');
-const theme = require('../src/modules/themeModule.js');
-// const grade = require('./modules/gradeModule.js');
-// const reminder = require('./modules/reminderModule.js');
-
-
-
-function globalFunction() {
-    
-    //#======================================================================================#//
-    //#================================|| GLOBAL VARIABLES ||================================#//
-    //#======================================================================================#//
-
-
-
-    const openLoginButton = document.querySelector('#openLoginButton');
-    const closeLoginButton = document.querySelector('#closeLoginButton');
-    const loginArea = document.querySelector('#loginArea');
-    const loginForm = document.querySelector('.loginForm');
-
-    const sideMenu = document.querySelector('.sideMenuArea');
-
-    const themeSwitch = document.querySelector('#themeSwitch');
-
-    const scheduleLink = document.querySelector('#scheduleLink');
-    const calendarLink = document.querySelector('#calendarLink');
-    const libraryLink = document.querySelector('#libraryLink');
-    const gradeLink = document.querySelector('#gradeLink');
-    const remindersLink = document.querySelector('#remindersLink');
-    const scholarGridLink = document.querySelector('#scholarGridLink');
-    const contactLink = document.querySelector('#contactLink');
-    const allLinks = [scheduleLink, calendarLink, libraryLink, gradeLink, remindersLink, scholarGridLink, contactLink];
-
-
-    
-    //#======================================================================================#//
-    //#====================================|| LOAD PAGE ||===================================#//
-    //#======================================================================================#//
-
-
-    //#======================================================================================#//
-    //#======================================|| LOGIN ||=====================================#//
-    //#======================================================================================#//
-
-
-    
-    openLoginButton.addEventListener('click', () => {
-        loginArea.style.display = 'block';
-    });
-
-    loginArea.addEventListener('click', (e) => {
-        const clickedElement = e.target;
-        if (clickedElement === loginArea) {
-            loginArea.style.display = 'none';
-        }
-    });
-
-    closeLoginButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        loginArea.style.display = 'none';
-    });
-
-
-
-    //#======================================================================================#//
-    //#==============================|| SIDEMENU NAVIGATION ||===============================#//
-    //#======================================================================================#//
-
-
-
-    function changeSelectedLink(element) {
-        allLinks.forEach(element => {
-            if (element.classList.contains('selectedLink')) {
-                element.classList.remove('selectedLink');
-            }
-        });
-        element.classList.add('selectedLink');
+if(accountFormChanger){
+  accountFormChanger.addEventListener('click', (e) => {
+    const el = e.target;
+    if(el.innerText === "Entrar") {
+      chooserBar.style.setProperty("margin-left", "0");
+      formSlider.firstElementChild.style.setProperty("margin-left", "0");
+    }else{
+      chooserBar.style.setProperty("margin-left", "50%");
+      formSlider.firstElementChild.style.setProperty("margin-left", "-100%");
     }
-
-    sideMenu.addEventListener('click', (e) => {
-
-        const clickedElement = e.target;
-        const clickedElementParent = clickedElement.parentElement;
-
-        if (clickedElement === scheduleLink || clickedElementParent === scheduleLink || clickedElement.firstChild === scheduleLink) {
-            changeSelectedLink(scheduleLink);
-        }
-        else if (clickedElement === calendarLink || clickedElementParent === calendarLink || clickedElement.firstChild === calendarLink) {
-            changeSelectedLink(calendarLink);
-        }
-        else if (clickedElement === libraryLink || clickedElementParent === libraryLink || clickedElement.firstChild === libraryLink) {
-            changeSelectedLink(libraryLink);
-        }
-        else if (clickedElement === gradeLink || clickedElementParent === gradeLink || clickedElement.firstChild === gradeLink) {
-            changeSelectedLink(gradeLink);
-        }
-        else if (clickedElement === remindersLink || clickedElementParent === remindersLink || clickedElement.firstChild === remindersLink) {
-            changeSelectedLink(remindersLink);
-        }
-        else if (clickedElement === scholarGridLink || clickedElementParent === scholarGridLink || clickedElement.firstChild === scholarGridLink) {
-            changeSelectedLink(scholarGridLink);
-        }
-        else if (clickedElement === contactLink || clickedElementParent === contactLink || clickedElement.firstChild === contactLink) {
-            changeSelectedLink(contactLink);
-        }
-    });
+  });
+}
 
 
 
-    //#======================================================================================#//
-    //#==================================|| THEME SWITCH ||==================================#//
-    //#======================================================================================#//
-
-
-
-    themeSwitch.addEventListener('click', () => {
-        theme.themeChanger();
-    });
-
-
-    
-    //#======================================================================================#//
-    //#=================================|| SCHEDULE SCREEN||=================================#//
-    //#======================================================================================#//
-
-
-
-    // lockTableButton.addEventListener('click', () => {
-    //     schedule.switchTableLock();
-    // });
-
-
-
-} globalFunction();
--- routes.js
+++ routes.js
@@ -1,61 +1,21 @@
 const express = require('express');
-const path = require('path');
-const handlebars = require('express-handlebars');
-const app = express();
+const route = express.Router();
 
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.use(express.static('.'));
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// const { loginRequired } = require('.src/middlewares/middleware');
 
-app.get('/', (req, res) => {
-    res.render('welcomeView', {
-        style: 'style.css'
-    });
-});
+// route.get('/', destructuringMiddlwares, exampleController.example);
 
-app.get('/horario-individual', (req, res) => {
-    res.render('scheduleView', {
-        style: 'style.css',
-        viewScript: 'scheduleModule.js'
-    });
-});
+route.get('/', homeController.render);
 
-app.get('/calendario', (req, res) => {
-    res.render('calendarView', {
-        style: 'style.css',
-        viewScript: 'scheduleModule.js'
-    });
-});
+route.get('/login', loginController.render);
+route.post('/login/login', loginController.login);
+route.post('/login/register', loginController.register);
+route.get('/login/logout', loginController.logout);
+route.get('/login/forgot', loginController.forgot);
 
-app.get('/biblioteca', (req, res) => {
-    res.render('libraryView', { style: 'style.css' });
-});
+// route.get('/logout', loginController.logout);
 
-app.get('/registro-notas', (req, res) => {
-    res.render('gradeView', {
-        style: 'style.css',
-        viewScript: 'gradeModule.js'
-    });
-});
-
-app.get('/lembretes', (req, res) => {
-    res.render('reminderView', {
-        style: 'style.css',
-        viewScript: 'reminderModule.js'
-    });
-});
-
-app.get('/matriz-escolar', (req, res) => {
-    res.render('scholarGridView', { style: 'style.css' });
-});
-
-app.get('/contatos', (req, res) => {
-    res.render('contactView', { style: 'style.css' });
-});
-
-
-
+module.exports = route;
